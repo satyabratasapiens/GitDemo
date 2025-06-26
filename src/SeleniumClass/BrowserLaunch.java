9b889364f9e865e1f48628757cab69dc8b67a5f0
satyabrata Sahoo <satyabrata.sahoo@sapiens.com>
2025-06-26 14:28:33 +0530

    My first commit

---
+++ src/SeleniumClass/BrowserLaunch.java
@@ -0,0 +1,15 @@
+package SeleniumClass;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
